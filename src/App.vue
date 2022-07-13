95c59dca561a1fb676d99ced77c323390fe4c4f6
DevContinue <s_kapahnke@web.de>
2022-07-13 08:51:56 +0200

    create header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   
-  
+  <Header />
   
 </template>
 
 <script>
-
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Header,
   },
 
   props: {
@@ -28,6 +28,8 @@ body {
   width: 100%;
   height: 100%;
   background-color: $second;
+  color: $ciWhite;
+  font-size: 20px;
 }
 
 * {
@@ -36,6 +38,24 @@ body {
   padding: 0;
 }
 
-  
+h1{
+  font-size: 50px;
+  line-height: 115%;
+}
+
+.subheading {
+  margin-top: 25px;
+  color: $ciGrey
+}
+
+@media (max-width: 800px) {
+  h1 {
+    font-size: 12vw;
+    text-align: center;
+  }
+  .subheading {
+    text-align: center;
+  }
+}
 
 </style>
